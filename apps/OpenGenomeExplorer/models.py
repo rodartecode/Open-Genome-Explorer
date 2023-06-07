7995bd83669fd057f4efbff73c8adde75301bcbd
Jesse <jerodarte@gmail.com>
2023-06-07 11:21:47 -0700

    Added USE_GCS setting and updated readme

--- apps/OpenGenomeExplorer/models.py
+++ apps/OpenGenomeExplorer/models.py
@@ -4,6 +4,7 @@ This file defines the database models
 
 import datetime
 import random
+import json
 from py4web.utils.populate import FIRST_NAMES, LAST_NAMES, IUP
 from .common import db, Field, auth
 from pydal.validators import *
@@ -51,6 +52,46 @@ db.define_table(
     Field('file_size', 'integer'),
     Field('confirmed', 'boolean', default=False))
 
+db. define_table(
+    "RSID",
+    Field('rsid', 'string', requires=IS_NOT_EMPTY()),
+    Field('last_updated', 'datetime', default=get_time),
+)
+
+db.define_table(
+    "RSID_Info",
+    Field('rsid', 'reference RSID'),
+    Field('url', 'string'),
+    Field('allele1', 'string'),
+    Field('allele2', 'string'),
+    Field('summary', 'text'),
+    Field('weight_of_evidence', 'integer'),
+    Field('last_updated', 'datetime', default=get_time),
+)
+
 db.commit()
 
 #db.SNP.truncate()
+import re
+
+def extract_final_pair(url):
+    match = re.search(r'\(([^)]+)\)$', url)
+    if match:
+        # Removing the semicolon and joining the characters.
+        return match.group(1).split(';')
+    else:
+        return None
+
+
+def init_db():
+    with open('good_snp_data.json', 'r') as f:
+        good_snps = json.load(f)
+    for rsid, info in good_snps:
+        inserted_rsid = db.RSID.update_or_insert(rsid=rsid)
+        if inserted_rsid:
+            for entry in info["snpedia"]:
+                url = entry["url"]
+                summary = entry["summary"]
+                allele1, allele2 = extract_final_pair(url)
+                new_info = dict(url=url, allele1=allele1, allele2=allele2, summary=summary)
+                db.RSID_Info.insert(rsid=inserted_rsid, **new_info)
