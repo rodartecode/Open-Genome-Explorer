c470238a0af457dc3c4e7fbdafd211a646ab6134
Jesse <jerodarte@gmail.com>
2023-06-08 22:42:20 -0700

    first pass at cloud deployment

--- deployment_tools/gae/main.py
+++ deployment_tools/gae/main.py
@@ -1,6 +1,5 @@
 import os
 import site
-import uuid
 site.addsitedir(os.path.join(os.path.dirname(__file__), 'lib'))
 from py4web.core import Reloader, bottle, Session
 os.environ['PY4WEB_DASHBOARD_MODE'] = 'demo'
