36557b2e0d516b0b6c9922db934ff0ce9ed19c58
Jesse <jerodarte@gmail.com>
2023-06-09 18:05:26 -0700

    more deployment troubleshooting

--- deployment_tools/gae/main.py
+++ deployment_tools/gae/main.py
@@ -7,12 +7,6 @@ os.environ['PY4WEB_DASHBOARD_MODE'] = 'demo'
 os.environ['PY4WEB_SERVICE_DB_URI'] = 'sqlite:memory'
 os.environ['PY4WEB_APPS_FOLDER'] = os.path.join(os.path.dirname(__file__), 'apps')
 os.environ['PY4WEB_SERVICE_FOLDER'] = os.path.join(os.path.dirname(__file__), 'apps/.service')
-if not os.path.exists(os.path.join(os.path.dirname(__file__), 'apps/.service')):
-    os.makedirs(os.path.join(os.path.dirname(__file__), 'apps/.service'))
-session_secret_filename = os.path.join(os.path.dirname(__file__), 'apps/.service/session.secret')
-if not os.path.exists(session_secret_filename):
-    with open(session_secret_filename, 'w') as f:
-        f.write(str(uuid.uuid4()))
 Session.SECRET = open(os.path.join(os.path.dirname(__file__), 'apps/.service/session.secret'), 'rb').read()
 Reloader.import_apps()
 app = bottle.default_app()
